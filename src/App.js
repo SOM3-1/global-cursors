46e91e0204cad7e8d93ad45364e9d39ceba1efcb
SOM3-1 <dush.gowda@gmail.com>
2024-12-07 00:43:15 -0600

    api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3001");
+const socket = io("https://cursors-backend.onrender.com");
 
 const App = () => {
   const [cursors, setCursors] = useState({});
